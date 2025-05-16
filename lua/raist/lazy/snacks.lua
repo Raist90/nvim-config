b58847d8a7a7b159aed8a2212abefcef24bcb44a
Manuel De Toma <manuel.detoma@gmail.com>
2025-05-16 23:16:38 +0200

    feat: add lualine.nvim

--- lua/raist/lazy/snacks.lua
+++ lua/raist/lazy/snacks.lua
@@ -13,20 +13,7 @@ return {
 		input = { enabled = true },
 		notifier = { enabled = true },
 		quickfile = { enabled = true },
-		statuscolumn = {
-			enabled = true,
-			left = { "mark", "sign" }, -- priority of signs on the left (high to low)
-			right = { "fold", "git" }, -- priority of signs on the right (high to low)
-			folds = {
-				open = false, -- show open fold icons
-				git_hl = false, -- use Git Signs hl for fold icons
-			},
-			git = {
-				-- patterns to match Git signs
-				patterns = { "GitSign", "MiniDiffSign" },
-			},
-			refresh = 50, -- refresh at most every 50ms
-		},
+		statuscolumn = { enabled = false },
 		words = { enabled = true },
 	},
 }
