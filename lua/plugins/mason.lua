2f9fc0b63a65a65c1f112610c5c23972197f6f68
Manuel De Toma <manuel.detoma@gmail.com>
2025-05-17 23:09:39 +0200

    fix: make volar work for both Vue 3 and Vue 2

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,50 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		opts = {
+			-- list of lsp for mason to install
+			ensure_installed = {
+				"html",
+				"cssls",
+				"ts_ls",
+				"eslint",
+				"jsonls",
+				"yamlls",
+				"tailwindcss",
+				"lua_ls",
+				"graphql",
+				"emmet_language_server",
+				"bashls",
+				"mdx_analyzer",
+			},
+		},
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		opts = {
+			-- list of formatter and linter for mason to install
+			ensure_installed = {
+				"prettierd", -- prettierd formatter
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"black", -- python formatter
+				"pylint", -- python linter
+				"shfmt", -- sh formatter with bash support
+			},
+		},
+	},
+}
