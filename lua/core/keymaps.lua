8be7f78646bcda7ed97f1017da5975923c4c8b26
Manuel De Toma <manuel.detoma@gmail.com>
2025-05-19 12:53:15 +0200

    feat: add some gitsigns keymap related to git blame

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -9,8 +9,14 @@ keymap("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
 keymap("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
 keymap("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
 
+-- Window management
 keymap("n", "<leader>wv", "<C-W>v", { desc = "Split Window Right", remap = true })
 keymap("n", "<leader>ww", "<C-W>w", { desc = "Next Window", remap = true })
 
+-- Package management
 keymap("n", "<leader>pm", "<cmd>Mason<cr>", { desc = "Mason" })
 keymap("n", "<leader>pl", "<cmd>Lazy<cr>", { desc = "Lazy" })
+
+-- Git
+keymap("n", "<leader>gb", "<cmd>Gitsigns blame_line<cr>", { desc = "Git Blame Line" })
+keymap("n", "<leader>gB", "<cmd>Gitsigns blame<cr>", { desc = "Git Blame" })
