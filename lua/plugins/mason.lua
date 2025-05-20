361bd1bb74789d18a61af3d66423c4dfddcef879
Manuel De Toma <manuel.detoma@gmail.com>
2025-05-20 13:54:05 +0200

    feat: remove prettierd from Mason ensure_installed

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,54 +1,53 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		opts = {
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		},
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = { "williamboman/mason.nvim" },
-		opts = {
-			-- list of lsp for mason to install
-			ensure_installed = {
-				"html",
-				"cssls",
-				"ts_ls",
-				"eslint",
-				"jsonls",
-				"yamlls",
-				"tailwindcss",
-				"lua_ls",
-				"graphql",
-				"emmet_language_server",
-				"bashls",
-				"mdx_analyzer",
-				"gopls",
-				"marksman",
-			},
-		},
-	},
-	{
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = { "williamboman/mason.nvim" },
-		opts = {
-			-- list of formatter and linter for mason to install
-			ensure_installed = {
-				"prettierd", -- prettierd formatter
-				"stylua", -- lua formatter
-				"isort", -- python formatter
-				"black", -- python formatter
-				"pylint", -- python linter
-				"shfmt", -- sh formatter with bash support
-			},
-		},
-	},
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    },
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = { "williamboman/mason.nvim" },
+    opts = {
+      -- list of lsp for mason to install
+      ensure_installed = {
+        "html",
+        "cssls",
+        "ts_ls",
+        "eslint",
+        "jsonls",
+        "yamlls",
+        "tailwindcss",
+        "lua_ls",
+        "graphql",
+        "emmet_language_server",
+        "bashls",
+        "mdx_analyzer",
+        "gopls",
+        "marksman",
+      },
+    },
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = { "williamboman/mason.nvim" },
+    opts = {
+      -- list of formatter and linter for mason to install
+      ensure_installed = {
+        "stylua", -- lua formatter
+        "isort",  -- python formatter
+        "black",  -- python formatter
+        "pylint", -- python linter
+        "shfmt",  -- sh formatter with bash support
+      },
+    },
+  },
 }
