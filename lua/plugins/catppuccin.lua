dc741318733db0360187218a8c37cc3887a4f661
Manuel De Toma <manuel.detoma@gmail.com>
2025-06-30 17:16:36 +0200

    feat: disable italics on catppuccin colorscheme

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,19 +4,13 @@ return {
   priority = 1000,
   config = function()
     require("catppuccin").setup({
-      styles = {
-        conditionals = {}
-      },
+      no_italic = true,
       transparent_background = true,
       integrations = {
         blink_cmp = {
           style = "bordered"
         },
         fzf = true,
-        -- telescope = {
-        --   enabled = true,
-        --   style = "nvchad",
-        -- },
         neotree = true,
         dashboard = true,
         notify = true,
