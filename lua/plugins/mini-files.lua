d00d5727dd90b0b08dab953239ada3daf90c208a
Manuel De Toma <manuel.detoma@gmail.com>
2025-08-29 09:09:31 +0200

    feat: migrate mini plugins to nvim-mini and update other plugins

--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -1,5 +1,5 @@
 return {
-  "echasnovski/mini.files",
+  "nvim-mini/mini.files",
   version = false,
   config = function()
     require("mini.files").setup({
--- lua/plugins/mini-pairs.lua
+++ lua/plugins/mini-pairs.lua
@@ -1,5 +1,5 @@
 return {
-  'echasnovski/mini.pairs',
+  'nvim-mini/mini.pairs',
   version = false,
   event = "InsertEnter",
   config = function()
