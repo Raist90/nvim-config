f84f3b653f4bf0591921fd8b88131edafb6b5648
Manuel De Toma <manuel.detoma@gmail.com>
2025-08-29 15:02:03 +0200

    feat: remove git-conflict plugin and rename git plugin into gitsigns

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,11 @@
+return {
+  -- Adds git related signs to the gutter, as well as utilities for managing changes
+  "lewis6991/gitsigns.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    preview_config = {
+      border = "single",
+    },
+    word_diff = true,
+  }
+}
