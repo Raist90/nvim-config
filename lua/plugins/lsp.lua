b8b0dd5ab7fd492bf52861e9e7e001a769424a81
Manuel De Toma <manuel.detoma@gmail.com>
2025-09-20 15:47:39 +0200

    refactor: clean lsp.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,11 +9,6 @@ return {
     vim.api.nvim_create_autocmd("LspAttach", {
       group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
       callback = function(event)
-        -- NOTE: Remember that Lua is a real programming language, and as such it is possible
-        -- to define small helper and utility functions so you don't have to repeat yourself.
-        --
-        -- In this case, we create a function that lets us more easily define mappings specific
-        -- for LSP related items. It sets the mode, buffer and description for us each time.
         local map = function(keys, func, desc, mode)
           mode = mode or "n"
           vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
@@ -30,50 +25,13 @@ return {
         --  Most Language Servers support renaming across files, etc.
         map("<leader>lr", vim.lsp.buf.rename, "Rename")
 
-        -- Execute a code action, usually your cursor needs to be on top of an error
-        -- or a suggestion from your LSP for this to activate.
-        -- map("<leader>la", vim.lsp.buf.code_action, "Code Action", { "n", "x" })
-
-        -- Find references for the word under your cursor.
-        -- map("gR", require("telescope.builtin").lsp_references, "Goto References")
-
-        -- Jump to the implementation of the word under your cursor.
-        --  Useful when your language has ways of declaring types without an actual implementation.
-        -- map("gI", require("telescope.builtin").lsp_implementations, "Goto Implementation")
-
-        -- Jump to the definition of the word under your cursor.
-        --  This is where a variable was first declared, or where a function is defined, etc.
-        --  To jump back, press <C-t>.
-        -- map("gd", require("telescope.builtin").lsp_definitions, "Goto Definition")
-
-        -- WARN: This is not Goto Definition, this is Goto Declaration.
-        --  For example, in C this would take you to the header.
-        -- map("gD", vim.lsp.buf.declaration, "Goto Declaration")
-
-        -- Fuzzy find all the symbols in your current document.
-        --  Symbols are things like variables, functions, types, etc.
-        -- map("<leader>fs", require("telescope.builtin").lsp_document_symbols, "Find Symbols")
-
-        -- Fuzzy find all the symbols in your current workspace.
-        --  Similar to document symbols, except searches over your entire project.
-        -- map("<leader>fS", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Find all Symbols")
-
-        -- Jump to the type of the word under your cursor.
-        --  Useful when you're not sure what type a variable is and you want to see
-        --  the definition of its *type*, not where it was *defined*.
-        -- map("gy", require("telescope.builtin").lsp_type_definitions, "Goto Type Definition")
-
         -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
         ---@param client vim.lsp.Client
         ---@param method vim.lsp.protocol.Method
         ---@param bufnr? integer some lsp support methods only in specific files
         ---@return boolean
         local function client_supports_method(client, method, bufnr)
-          if vim.fn.has("nvim-0.11") == 1 then
-            return client:supports_method(method, bufnr)
-          else
-            return client.supports_method(method, { bufnr = bufnr })
-          end
+          return client:supports_method(method, bufnr)
         end
 
         -- The following two autocommands are used to highlight references of the
@@ -187,7 +145,7 @@ return {
           hybridMode = false,
         },
       },
-      on_attach = function(client, _bufnr)
+      on_attach = function(client)
         -- Disable formatting capabilities for Volar
         client.server_capabilities.documentFormattingProvider = false
       end,
