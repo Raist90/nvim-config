a8c9241197030163fd49b2214b7c683a0c0533cb
Manuel De Toma <manuel.detoma@gmail.com>
2025-11-05 22:52:03 +0100

    refactor: clean more keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,40 +1,47 @@
 vim.g.mapleader = " " -- Set leader key before Lazy
 vim.g.maplocalleader = " "
 
-local keymap = vim.keymap.set
+local map = require("util").map
 
-keymap("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window" })
-keymap("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window" })
-keymap("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window" })
-keymap("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window" })
+map("<C-h>", "<C-w>h", "Go to Left Window")
+map("<C-j>", "<C-w>j", "Go to Lower Window")
+map("<C-k>", "<C-w>k", "Go to Upper Window")
+map("<C-l>", "<C-w>l", "Go to Right Window")
 
-keymap("n", "<leader>pm", "<cmd>Mason<cr>", { desc = "Mason" })
-keymap("n", "<leader>pl", "<cmd>Lazy<cr>", { desc = "Lazy" })
+map("<leader>pm", "<cmd>Mason<cr>", "Mason")
+map("<leader>pl", "<cmd>Lazy<cr>", "Lazy")
 
-keymap("n", "<leader>gb", "<cmd>Gitsigns blame_line<cr>", { desc = "Git Blame Line" })
-keymap("n", "<leader>gB", "<cmd>Gitsigns blame<cr>", { desc = "Git Blame" })
+map("<leader>gb", "<cmd>Gitsigns blame_line<cr>", "Git Blame Line")
+map("<leader>gB", "<cmd>Gitsigns blame<cr>", "Git Blame")
 
-keymap("n", "<leader>qo", function()
+map("<leader>qo", function()
   vim.cmd("copen")
-end, { desc = "Open Quickfix List" })
+end, "Open Quickfix List")
 
-keymap("n", "<leader>qc", function()
+map("<leader>qc", function()
   vim.cmd("cclose")
-end, { desc = "Close Quickfix List" })
+end, "Close Quickfix List")
 
-keymap("n", "<leader>nh", function()
+map("<leader>nh", function()
   require("snacks").notifier.show_history()
-end, { desc = "Show Notification History" })
+end, "Show Notification History")
 
-keymap("n", "<leader>Z", function()
+map("<leader>Z", function()
   require("snacks").zen()
-end, { desc = "Toggle Zen Mode" })
+end, "Toggle Zen Mode")
 
-keymap("n", "<leader>ld", function()
+map("<leader>ld", function()
   vim.diagnostic.open_float(nil, { focusable = true, scope = "cursor" })
-end, { desc = "Open Diagnostic Float" })
+end, "Open Diagnostic Float")
 
-keymap("n", "<leader>Co", "<cmd>CopilotChatOpen<cr>", { desc = "Copilot Chat Prompts" })
-vim.keymap.set("n", "<leader>Cc", "<cmd>CopilotChatClose<cr>", { desc = "Copilot Chat Close" })
+map("<leader>Co", function()
+  require("CopilotChat").open()
+end, "Copilot Chat Prompts")
 
-keymap("n", "<leader>tt", "<cmd>terminal<cr>", { desc = "Open Terminal" })
+map("<leader>Cc", function()
+  require("CopilotChat").close()
+end, "Copilot Chat Close")
+
+map("<leader>tt", function()
+  vim.cmd("terminal")
+end, "Open Terminal")
--- lua/util.lua
+++ lua/util.lua
@@ -1,5 +1,9 @@
 local M = {}
 
+---@param keys string
+---@param func string|function
+---@param desc string
+---@param mode? string
 local map = function(keys, func, desc, mode)
   mode = mode or "n"
   vim.keymap.set(mode, keys, func, { desc = desc })
