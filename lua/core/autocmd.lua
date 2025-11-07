40b8af3e18f6917d45147f1f3476bfa14117cbc6
Manuel De Toma <manuel.detoma@gmail.com>
2025-11-07 09:46:57 +0100

    feat: update autocmds
    feat: update autocmds

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -1,8 +1,23 @@
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking text",
+  pattern = "*",
+  desc = "Highlight selection on yank",
   callback = function()
-    vim.highlight.on_yank()
+    vim.hl.on_yank()
+  end,
+})
+
+-- Open help files in a vertical split
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "help",
+  command = "wincmd L",
+})
+
+-- Disable auto commenting on new lines
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("No auto comment", {}),
+  callback = function()
+    vim.opt_local.formatoptions:remove({ "c", "r", "o" })
   end,
 })
 
