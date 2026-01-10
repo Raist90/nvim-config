1a61a65ef78fe5ef4fa61dd54c6be09244de0258
Manuel De Toma <manuel.detoma@gmail.com>
2026-01-10 16:26:06 +0100

    feat: migrate to nvim-treesitter main branch
    feat: migrate to nvim-treesitter main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,21 +1,56 @@
+-- https://mhpark.me/posts/update-treesitter-main/
 return {
   "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
+  lazy = false,
   build = ":TSUpdate",
+  branch = "main",
   config = function()
-    local configs = require("nvim-treesitter.configs")
+    local ts = require("nvim-treesitter")
+    local parsers = {
+      "bash",
+      "comment",
+      "css",
+      "diff",
+      "dockerfile",
+      "git_config",
+      "gitcommit",
+      "gitignore",
+      "go",
+      "html",
+      "http",
+      "javascript",
+      "jsdoc",
+      "json",
+      "json5",
+      "jsonc",
+      "lua",
+      "make",
+      "markdown",
+      "markdown_inline",
+      "regex",
+      "scss",
+      "sql",
+      "toml",
+      "tsx",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "yaml",
+    }
 
-    configs.setup({
-      ensure_installed = {
-        "go",
-        "lua",
-        "javascript",
-        "html",
-        "typescript",
-      },
-      sync_install = false,
-      highlight = { enable = true },
-      auto_install = true,
+    for _, parser in ipairs(parsers) do
+      ts.install(parser)
+    end
+
+    vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+    vim.wo[0][0].foldmethod = "expr"
+    vim.api.nvim_command("set nofoldenable")
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = parsers,
+      callback = function()
+        vim.treesitter.start()
+      end,
     })
   end,
 }
