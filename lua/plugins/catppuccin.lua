d00e9462f7505ad72ebeebdba080b73c993a78b1
Manuel De Toma <manuel.detoma@gmail.com>
2026-02-07 14:43:22 +0100

    feat: back to catppuccin

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,58 @@
+return {
+  "catppuccin/nvim",
+  enabled = true,
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      float = {
+        solid = true,
+        transparent = true,
+      },
+      no_italic = true,
+      transparent_background = true,
+      integrations = {
+        blink_cmp = {
+          style = "bordered",
+        },
+        diffview = true,
+        fzf = true,
+        neotree = true,
+        dashboard = true,
+        notify = true,
+        treesitter = true,
+        which_key = true,
+        gitsigns = {
+          enabled = true,
+          transparent = true,
+        },
+        mason = true,
+        mini = {
+          enabled = true,
+        },
+        snacks = {
+          enabled = true,
+          indent_scope_color = "lavender",
+        },
+        flash = true,
+      },
+      custom_highlights = function(colors)
+        return {
+          LazyGitBorder = { fg = colors.lavender },
+
+          WinbarActive = { fg = colors.blue, style = { "bold" } },
+          WinbarInactive = { fg = colors.subtext0 },
+
+          Statusline = { fg = colors.text },
+          StatuslineGit = { fg = colors.blue, style = { "bold" } },
+          StatuslineError = { fg = colors.red },
+          StatuslineWarn = { fg = colors.yellow },
+          StatuslineInfo = { fg = colors.sky },
+          StatuslineHint = { fg = colors.teal },
+          StatuslineTime = { fg = colors.blue, style = { "bold" } },
+        }
+      end,
+    })
+    vim.cmd("colorscheme catppuccin-macchiato")
+  end,
+}
--- lua/plugins/nightfox.lua
+++ lua/plugins/nightfox.lua
@@ -1,5 +1,6 @@
 return {
   "EdenEast/nightfox.nvim",
+  enabled = false,
   lazy = false,
   priority = 1000,
   config = function()
