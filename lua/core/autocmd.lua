b6162e1951390ae6d30162b46ab608a98143abfa
Manuel De Toma <manuel.detoma@gmail.com>
2026-04-03 16:28:49 +0200

    feat: add cmd to resize split when resizing window

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -37,3 +37,12 @@ vim.api.nvim_create_autocmd("User", {
     vim.b.copilot_suggestion_hidden = false
   end,
 })
+
+-- Resize splits if window got resized
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd("tabdo wincmd =")
+    vim.cmd("tabnext " .. current_tab)
+  end,
+})
